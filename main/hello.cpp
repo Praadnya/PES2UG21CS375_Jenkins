b53e900d1d40dded19bbd6e624d6215494be361c
Praadnya <99022986+Praadnya@users.noreply.github.com>
2024-03-08 13:20:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run. This is PES2UG21CS375\n";
+  cout << "I have successfully built and run.\n";
+  cout << "Webhook successfully enabled\n";
   return 0;
 }
